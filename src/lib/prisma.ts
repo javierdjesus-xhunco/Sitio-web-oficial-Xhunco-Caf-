2ccf11fe402b0fa5565075c4fc269bc58a65ad62
Javier De Jesús <javierdjesus@gmail.com>
2026-01-27 16:49:05 -0600

    Guarda cambios locales

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL is not set (revisa tu .env y Vercel env vars)");
-}
-
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    // log: ["query", "error", "warn"],
+    // log: ["query", "error", "warn"], // opcional
   });
 
 if (process.env.NODE_ENV !== "production") {
