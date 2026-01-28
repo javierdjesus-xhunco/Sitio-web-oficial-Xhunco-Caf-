e12fc1e16adebc76bd897c1b289a340180d32189
Javier De Jesús <javierdjesus@gmail.com>
2026-01-28 12:42:28 -0600

    Fix tailwind config (resolve conflict)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
-const config = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
